f6147b2dc1886a94d7cf0666b91c5b289c4fb531
arkiver <arkiver@protonmail.com>
2025-05-18 14:17:14 +0200

    Version 20250518.04. Accept 302 and 301.

--- twitch.lua
+++ twitch.lua
@@ -903,6 +903,8 @@ wget.callbacks.write_to_warc = function(url, http_stat)
   end
   if http_stat["statcode"] ~= 200
     and http_stat["statcode"] ~= 404
+    and http_stat["statcode"] ~= 302
+    and http_stat["statcode"] ~= 301
     and (
       http_stat["statcode"] ~= 403
       or not string.match(url["url"], "^https://[^%.]+%.cloudfront%.net/.+/storyboards/[0-9]+%-[a-z]+%-?[0-9]?%.j[ps][go]n?$")
