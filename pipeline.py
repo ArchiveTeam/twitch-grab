ba5ab68c0b8afb7310d3d433161a9a2161ce5a91
arkiver <arkiver@protonmail.com>
2025-07-15 12:45:01 +0200

    Version 20250715.02. Turn back WARC temp dir change.

--- pipeline.py
+++ pipeline.py
@@ -77,7 +77,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250715.01'
+VERSION = '20250715.02'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0'
 TRACKER_ID = 'twitch'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -286,7 +286,7 @@ class WgetArgs(object):
             '--warc-dedup-url-agnostic',
             '--warc-compression-use-zstd',
             '--warc-zstd-dict-no-include',
-            '--warc-tempdir', ItemInterpolation('%(item_dir)s'),
+            #'--warc-tempdir', ItemInterpolation('%(item_dir)s'),
         ]
         dict_data = ZstdDict.get_dict()
         with open(os.path.join(item['item_dir'], 'zstdict'), 'wb') as f:
